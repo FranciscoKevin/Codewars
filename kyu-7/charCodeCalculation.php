39ee1432ceb960075d54cdefa0c15244231fa803
FrKev1 <francisco.kevin37@gmail.com>
2022-03-01 22:14:08 +0100

    Kata kuy 7

---
+++ kyu-7/charCodeCalculation.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+    DESCRIPTION
+
+    Given a string, turn each character into its ASCII character code.
+    Join them together to create a number - let's call this number total1 :
+    
+    'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+    Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+    total1 = 656667
+                ^
+    total2 = 656661
+                ^
+    Then return the difference between the sum of the digits in total1 and total2:
+
+    (6 + 5 + 6 + 6 + 6 + 7)
+    - (6 + 5 + 6 + 6 + 6 + 1)
+    -------------------------
+                        6
+    
+*/
+
+function calc(string $string): int
+{
+    $total1 = implode(array_map("ord", str_split($string)));
+    $total2 = str_replace("7", "1", $total1);
+
+    return array_sum(str_split($total1)) - array_sum(str_split($total2));
+}
