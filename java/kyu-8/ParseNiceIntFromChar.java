9122d19ebaff0ee7aa4083f040267d7fac4d98c7
FrKev1 <francisco.kevin37@gmail.com>
2022-04-01 23:36:09 +0200

    Kata kyu 8

---
+++ java/kyu-8/ParseNiceIntFromChar.java
@@ -0,0 +1,19 @@
+/*
+    DESCRIPTION
+
+    You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+    Write a program that returns the girl's age (0-9) as an integer.
+
+    Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". 
+    The first character in the string is always a number.
+*/
+
+
+public class CharProblem {
+  public static int howOld(final String herOld) {
+    String str = herOld.substring(0, 1);
+    int number = Integer.parseInt(str);  
+    return number;
+  }
+}
